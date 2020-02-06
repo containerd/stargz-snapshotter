531e9995634b9d28d436bb6e21a32cf372b71fdd
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2020-02-06 16:05:02 +0900

    Fix Copyright notice
    Stargz Snapshotter is partially based on CRFS(for filesystem) and stargzify(for
    ctr-remote optimize subcommand) by Go Authors.
    We need additional Copyright notice for these codes.
    
    CRFS project repo:
    - https://github.com/google/crfs
    stargzify implementation:
    - https://github.com/google/crfs/tree/master/stargz/stargzify
    
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -1,5 +1,6 @@
 /*
    Copyright The containerd Authors.
+   Copyright 2019 The Go Authors.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
--- cmd/ctr-remote/sampler/sampler.go
+++ cmd/ctr-remote/sampler/sampler.go
@@ -1,5 +1,6 @@
 /*
    Copyright The containerd Authors.
+   Copyright 2013-2018 Docker, Inc.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
