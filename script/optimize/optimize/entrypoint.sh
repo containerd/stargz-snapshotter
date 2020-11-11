2966f943a07c5b6a569af5c7c842fe9c6d19ed46
ktock <ktokunaga.mail@gmail.com>
2020-11-12 00:17:15 +0900

    Support CNI-based networking for optimization
    Currently, the container created by `ctr-remote` during optimization doesn't
    have configured networking interfaces.
    
    This commit enables `ctr-remote` to setup container's interface using CNI
    plugins and configs installed on the system or ones specified by the command
    line options. This commit also includes the ability to configure `/etc/hosts`
    and `/etc/resolv.conf` (8.8.8.8 is used as the default nameserver).
    
    The test uses CNI project's `bridge` plugin and tries to downaload contents from
    example.com.
    
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>

--- script/optimize/optimize/entrypoint.sh
+++ script/optimize/optimize/entrypoint.sh
@@ -161,4 +161,100 @@ validate_toc_json "${WORKING_DIR}/dist-manifest.json" "0" "${LAYER_0}"
 validate_toc_json "${WORKING_DIR}/dist-manifest.json" "1" "${LAYER_1}"
 validate_toc_json "${WORKING_DIR}/dist-manifest.json" "2" "${LAYER_2}"
 
+# Test networking & mounting work
+
+# Make bridge plugin manipulate iptables instead of nftables as this test runs
+# in a Docker container that network is configured with iptables.
+# c.f. https://github.com/moby/moby/issues/26824
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+
+# Install CNI plugins and configs to the customized paths (not /opt/cni/bin and /etc/cni/net.d)
+mkdir /tmp/cni/ /tmp/bin/
+curl -Ls https://github.com/containernetworking/plugins/releases/download/v0.8.7/cni-plugins-linux-amd64-v0.8.7.tgz | tar xzv -C /tmp/bin
+cat <<'EOF' > /tmp/cni/test.conflist
+{
+  "cniVersion": "0.4.0",
+  "name": "test",
+  "plugins" : [{
+    "type": "bridge",
+    "bridge": "test0",
+    "isDefaultGateway": true,
+    "forceAddress": false,
+    "ipMasq": true,
+    "hairpinMode": true,
+    "ipam": {
+      "type": "host-local",
+      "subnet": "10.10.0.0/16"
+    }
+  },
+  {
+    "type": "loopback"
+  }]
+}
+EOF
+
+# Try to connect to the internet from the container
+TESTDIR=$(mktemp -d)
+/tmp/out/ctr-remote i optimize \
+                    --period=20 \
+                    --cni \
+                    --cni-plugin-conf-dir='/tmp/cni' \
+                    --cni-plugin-dir='/tmp/bin' \
+                    --add-hosts='testhost:1.2.3.4,test2:5.6.7.8' \
+                    --dns-nameservers='8.8.8.8' \
+                    --mount="type=bind,src=${TESTDIR},dst=/mnt,options=bind" \
+                    --entrypoint='[ "/bin/bash", "-c" ]' \
+                    --args='[ "curl example.com > /mnt/result_page && ip a show dev eth0 ; echo -n $? > /mnt/if_exists && ip a > /mnt/if_info && cat /etc/hosts > /mnt/hosts" ]' \
+                    ghcr.io/stargz-containers/centos:8-test "${REGISTRY_HOST}:5000/test:1"
+
+# Check if all contents are successfuly passed
+if ! [ -f "${TESTDIR}/if_exists" ] || \
+        ! [ -f "${TESTDIR}/result_page" ] || \
+        ! [ -f "${TESTDIR}/if_info" ] || \
+        ! [ -f "${TESTDIR}/hosts" ]; then
+    echo "the result files not found; bind-mount might not work"
+    exit 1
+fi
+
+# Check if /etc/hosts contains expected contents
+if [ "$(cat ${TESTDIR}/hosts | grep testhost | sed -E 's/([0-9.]*).*/\1/')" != "1.2.3.4" ] || \
+       [ "$(cat ${TESTDIR}/hosts | grep test2 | sed -E 's/([0-9.]*).*/\1/')" != "5.6.7.8" ]; then
+    echo "invalid contents in /etc/hosts"
+    cat "${TESTDIR}/hosts"
+    exit 1
+fi
+echo "hosts configured:"
+cat "${TESTDIR}/hosts"
+
+# Check if the interface is created by the bridge plugin
+if [ "$(cat ${TESTDIR}/if_exists)" != "0" ] ; then
+    echo "interface didn't configured:"
+    cat "${TESTDIR}/if_exists"
+    echo "interface info:"
+    cat "${TESTDIR}/if_info"
+    exit 1
+fi
+echo "Interface created:"
+cat "${TESTDIR}/if_info"
+
+# Check if the contents are downloaded from the internet
+SAMPLE_PAGE=$(mktemp)
+curl example.com > "${SAMPLE_PAGE}"
+if ! [ -s "${SAMPLE_PAGE}" ] ; then
+    echo "sample page file is empty; failed to get the contents of example.com; check the internet connection"
+    exit 1
+fi
+echo "sample contents of example.com"
+cat "${SAMPLE_PAGE}"
+SAMPLE_PAGE_SHA256=$(cat "${SAMPLE_PAGE}" | sha256sum | sed -E 's/([^ ]*).*/sha256:\1/g')
+RESULT_PAGE_SHA256=$(cat "${TESTDIR}/result_page" | sha256sum | sed -E 's/([^ ]*).*/sha256:\1/g')
+if [ "${SAMPLE_PAGE_SHA256}" != "${RESULT_PAGE_SHA256}" ] ; then
+    echo "failed to get expected contents from the internet, inside the container: ${SAMPLE_PAGE_SHA256} != ${RESULT_PAGE_SHA256}"
+    echo "got contetns:"
+    cat "${TESTDIR}/result_page"
+    exit 1
+fi
+echo "expected contents successfly downloaded from the internet, in the container. contents:"
+cat "${TESTDIR}/result_page"
+
 exit 0
--- vendor/github.com/containerd/containerd/cmd/ctr/commands/commands.go
+++ vendor/github.com/containerd/containerd/cmd/ctr/commands/commands.go
@@ -123,6 +123,10 @@ var (
 			Usage: "runtime name",
 			Value: defaults.DefaultRuntime,
 		},
+		cli.StringFlag{
+			Name:  "runtime-config-path",
+			Usage: "optional runtime config path",
+		},
 		cli.BoolFlag{
 			Name:  "tty,t",
 			Usage: "allocate a TTY for the container",
@@ -154,6 +158,18 @@ var (
 		cli.BoolFlag{
 			Name:  "seccomp",
 			Usage: "enable the default seccomp profile",
+		},
+		cli.StringFlag{
+			Name:  "seccomp-profile",
+			Usage: "file path to custom seccomp profile. seccomp must be set to true, before using seccomp-profile",
+		},
+		cli.StringFlag{
+			Name:  "apparmor-default-profile",
+			Usage: "enable AppArmor with the default profile with the specified name, e.g. \"cri-containerd.apparmor.d\"",
+		},
+		cli.StringFlag{
+			Name:  "apparmor-profile",
+			Usage: "enable AppArmor with an existing custom profile",
 		},
 	}
 )
