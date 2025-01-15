containerd/stargz-snapshotter

commit 985b021b414ae5b26a8709a45d711ac61da324c0

go.mod : prevernt go upgraded to 1.23
Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>

cmd/go.mod
@@ -1,8 +1,6 @@
 module github.com/containerd/stargz-snapshotter/cmd
 
-go 1.23.0
-
-toolchain go1.23.2
+go 1.22.7
 
 require (
 	github.com/containerd/containerd/api v1.8.0
@@ -28,7 +26,7 @@ require (
 	golang.org/x/sync v0.9.0
 	golang.org/x/sys v0.26.0
 	google.golang.org/grpc v1.68.0
-	k8s.io/cri-api v0.32.0-beta.0
+	k8s.io/cri-api v0.32.0-alpha.0
 )
 
 require (

go.mod
@@ -1,8 +1,6 @@
 module github.com/containerd/stargz-snapshotter
 
-go 1.23.0
-
-toolchain go1.23.2
+go 1.22.7
 
 require (
 	github.com/containerd/console v1.0.4
@@ -34,7 +32,7 @@ require (
 	k8s.io/api v0.31.2
 	k8s.io/apimachinery v0.31.2
 	k8s.io/client-go v0.31.2
-	k8s.io/cri-api v0.32.0-beta.0
+	k8s.io/cri-api v0.32.0-alpha.0
 )
 
 require (

ipfs/go.mod
@@ -2,8 +2,6 @@ module github.com/containerd/stargz-snapshotter/ipfs
 
 go 1.22.7
 
-toolchain go1.23.2
-
 require (
 	github.com/containerd/containerd/v2 v2.0.0
 	github.com/containerd/platforms v1.0.0-rc.0

cmd/go.sum
@@ -502,8 +502,8 @@ k8s.io/apimachinery v0.31.2 h1:i4vUt2hPK56W6mlT7Ry+AO8eEsyxMD1U44NR22CLTYw=
 k8s.io/apimachinery v0.31.2/go.mod h1:rsPdaZJfTfLsNJSQzNHQvYoTmxhoOEofxtOsF3rtsMo=
 k8s.io/client-go v0.31.2 h1:Y2F4dxU5d3AQj+ybwSMqQnpZH9F30//1ObxOKlTI9yc=
 k8s.io/client-go v0.31.2/go.mod h1:NPa74jSVR/+eez2dFsEIHNa+3o09vtNaWwWwb1qSxSs=
-k8s.io/cri-api v0.32.0-beta.0 h1:jEzxyqoSUuUn99naSmlRdepmSa38UGj0QNxZdQeGgMY=
-k8s.io/cri-api v0.32.0-beta.0/go.mod h1:rwEJEDa3cV0bT4AZ8en5LbBEAwghbAdDkLNHz//zwsk=
+k8s.io/cri-api v0.32.0-alpha.0 h1:Rs9prajcHWZAdy9ueQdD2R+OOnDD3rKYbM9hQ90iEQU=
+k8s.io/cri-api v0.32.0-alpha.0/go.mod h1:Po3TMAYH/+KrZabi7QiwQI4a692oZcUOUThd/rqwxrI=
 k8s.io/klog/v2 v2.130.1 h1:n9Xl7H1Xvksem4KFG4PYbdQCQxqc/tTUyrgXaOhHSzk=
 k8s.io/klog/v2 v2.130.1/go.mod h1:3Jpz1GvMt720eyJH1ckRHK1EDfpxISzJ7I9OYgaDtPE=
 k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 h1:BZqlfIlq5YbRMFko6/PM7FjZpUb45WallggurYhKGag=

go.sum
@@ -429,8 +429,8 @@ k8s.io/apimachinery v0.31.2 h1:i4vUt2hPK56W6mlT7Ry+AO8eEsyxMD1U44NR22CLTYw=
 k8s.io/apimachinery v0.31.2/go.mod h1:rsPdaZJfTfLsNJSQzNHQvYoTmxhoOEofxtOsF3rtsMo=
 k8s.io/client-go v0.31.2 h1:Y2F4dxU5d3AQj+ybwSMqQnpZH9F30//1ObxOKlTI9yc=
 k8s.io/client-go v0.31.2/go.mod h1:NPa74jSVR/+eez2dFsEIHNa+3o09vtNaWwWwb1qSxSs=
-k8s.io/cri-api v0.32.0-beta.0 h1:jEzxyqoSUuUn99naSmlRdepmSa38UGj0QNxZdQeGgMY=
-k8s.io/cri-api v0.32.0-beta.0/go.mod h1:rwEJEDa3cV0bT4AZ8en5LbBEAwghbAdDkLNHz//zwsk=
+k8s.io/cri-api v0.32.0-alpha.0 h1:Rs9prajcHWZAdy9ueQdD2R+OOnDD3rKYbM9hQ90iEQU=
+k8s.io/cri-api v0.32.0-alpha.0/go.mod h1:Po3TMAYH/+KrZabi7QiwQI4a692oZcUOUThd/rqwxrI=
 k8s.io/klog/v2 v2.130.1 h1:n9Xl7H1Xvksem4KFG4PYbdQCQxqc/tTUyrgXaOhHSzk=
 k8s.io/klog/v2 v2.130.1/go.mod h1:3Jpz1GvMt720eyJH1ckRHK1EDfpxISzJ7I9OYgaDtPE=
 k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 h1:BZqlfIlq5YbRMFko6/PM7FjZpUb45WallggurYhKGag=

commit b05c75e8c9e88662f83683180c6e13b68e494f22

*/go.mod: use go1.22.0
Noticed that 985b021b414ae5b26a8709a45d711ac61da324c0 caused buildkit
to have to update its go.mod to go1.22.7, and we're trying to keep
it stable at 1.22.0 as minimum.

Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

cmd/go.mod
@@ -1,6 +1,6 @@
 module github.com/containerd/stargz-snapshotter/cmd
 
-go 1.22.7
+go 1.22.0
 
 require (
 	github.com/containerd/containerd/api v1.8.0

go.mod
@@ -1,6 +1,6 @@
 module github.com/containerd/stargz-snapshotter
 
-go 1.22.7
+go 1.22.0
 
 require (
 	github.com/containerd/console v1.0.4

ipfs/go.mod
@@ -1,6 +1,6 @@
 module github.com/containerd/stargz-snapshotter/ipfs
 
-go 1.22.7
+go 1.22.0
 
 require (
 	github.com/containerd/containerd/v2 v2.0.2