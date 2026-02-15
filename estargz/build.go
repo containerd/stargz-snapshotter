containerd/stargz-snapshotter

commit cf6d7a03bce843d1f5e814268be141c45e5511b8

Replace the multierror dependency with a simple error aggregator.
This lets functions that want to aggregate and return a collection of errors use a `[]error` to accumulate them, and `errorutil.Aggregate(list)` to turn them into a single `error`.  If the list is empty, the `error` will be nil.  If the list is a singleton, it will return the single error.  Otherwise, it will construct a new error combining the constituent error messages.

Fixes: https://github.com/containerd/stargz-snapshotter/issues/205
Signed-off-by: Matt Moore <mattmoor@vmware.com>

estargz/build.go
@@ -36,7 +36,7 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/hashicorp/go-multierror"
+	"github.com/containerd/stargz-snapshotter/estargz/errorutil"
 	digest "github.com/opencontainers/go-digest"
 	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
@@ -431,19 +431,20 @@ func (tf *tempFiles) TempFile(dir, pattern string) (*os.File, error) {
 	return f, nil
 }
 
-func (tf *tempFiles) CleanupAll() (allErr error) {
+func (tf *tempFiles) CleanupAll() error {
 	tf.filesMu.Lock()
 	defer tf.filesMu.Unlock()
+	var allErr []error
 	for _, f := range tf.files {
 		if err := f.Close(); err != nil {
-			allErr = multierror.Append(allErr, err)
+			allErr = append(allErr, err)
 		}
 		if err := os.Remove(f.Name()); err != nil {
-			allErr = multierror.Append(allErr, err)
+			allErr = append(allErr, err)
 		}
 	}
 	tf.files = nil
-	return nil
+	return errorutil.Aggregate(allErr)
 }
 
 func newCountReader(r io.ReaderAt) (*countReader, error) {

estargz/errorutil/errors.go
@@ -1,0 +1,40 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package errorutil
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+// Aggregate combines a list of errors into a single new error.
+func Aggregate(errs []error) error {
+	switch len(errs) {
+	case 0:
+		return nil
+	case 1:
+		return errs[0]
+	default:
+		points := make([]string, len(errs)+1)
+		points[0] = fmt.Sprintf("%d error(s) occurred:", len(errs))
+		for i, err := range errs {
+			points[i+1] = fmt.Sprintf("* %s", err)
+		}
+		return errors.New(strings.Join(points, "\n\t"))
+	}
+}

estargz/estargz.go
@@ -42,7 +42,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/hashicorp/go-multierror"
+	"github.com/containerd/stargz-snapshotter/estargz/errorutil"
 	digest "github.com/opencontainers/go-digest"
 	"github.com/pkg/errors"
 )
@@ -739,11 +739,13 @@ func footerBytes(tocOff int64) []byte {
 }
 
 func parseFooter(p []byte) (tocOffset int64, footerSize int64, rErr error) {
+	var allErr []error
+
 	tocOffset, err := parseEStargzFooter(p)
 	if err == nil {
 		return tocOffset, FooterSize, nil
 	}
-	rErr = multierror.Append(rErr, err)
+	allErr = append(allErr, err)
 
 	pad := len(p) - legacyFooterSize
 	if pad < 0 {
@@ -753,8 +755,7 @@ func parseFooter(p []byte) (tocOffset int64, footerSize int64, rErr error) {
 	if err == nil {
 		return tocOffset, legacyFooterSize, nil
 	}
-	rErr = multierror.Append(rErr, err)
-	return
+	return 0, 0, errorutil.Aggregate(append(allErr, err))
 }
 
 func parseEStargzFooter(p []byte) (tocOffset int64, err error) {

estargz/errorutil/errors_test.go
@@ -1,0 +1,57 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package errorutil
+
+import (
+	"testing"
+
+	"github.com/pkg/errors"
+)
+
+func TestNoError(t *testing.T) {
+	if err := Aggregate(nil); err != nil {
+		t.Errorf("Aggregate(nil) = %v, wanted nil", err)
+	}
+	if err := Aggregate([]error{}); err != nil {
+		t.Errorf("Aggregate(nil) = %v, wanted nil", err)
+	}
+}
+
+func TestOneError(t *testing.T) {
+	want := errors.New("this is A random Error string")
+	if got := Aggregate([]error{want}); got != want {
+		t.Errorf("Aggregate() = %v, wanted %v", got, want)
+	}
+}
+
+func TestMultipleErrors(t *testing.T) {
+	want := `3 error(s) occurred:
+	* foo
+	* bar
+	* baz`
+
+	err := Aggregate([]error{
+		errors.New("foo"),
+		errors.New("bar"),
+		errors.New("baz"),
+	})
+	if err == nil {
+		t.Errorf("Aggregate() = nil, wanted %s", want)
+	} else if got := err.Error(); got != want {
+		t.Errorf("Aggregate() = %s, wanted %s", got, want)
+	}
+}

estargz/go.mod
@@ -3,7 +3,6 @@ module github.com/containerd/stargz-snapshotter/estargz
 go 1.13
 
 require (
-	github.com/hashicorp/go-multierror v1.1.0
 	github.com/opencontainers/go-digest v1.0.0
 	github.com/pkg/errors v0.9.1
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a

estargz/go.sum
@@ -1,7 +1,3 @@
-github.com/hashicorp/errwrap v1.0.0 h1:hLrqtEDnRye3+sgx6z4qVLNuviH3MR5aQ0ykNJa/UYA=
-github.com/hashicorp/errwrap v1.0.0/go.mod h1:YH+1FKiLXxHSkmPseP+kNlulaMuP3n2brvKWEqk/Jc4=
-github.com/hashicorp/go-multierror v1.1.0 h1:B9UzwGQJehnUY1yNrnwREHc3fGbC2xefo8g4TbElacI=
-github.com/hashicorp/go-multierror v1.1.0/go.mod h1:spPvp8C1qA32ftKqdAHm4hHTbPw+vmowP0z+KUhOZdA=
 github.com/opencontainers/go-digest v1.0.0 h1:apOUWs51W5PlhuyGyz9FCeeBIOUDA/6nW8Oi/yOhh5U=
 github.com/opencontainers/go-digest v1.0.0/go.mod h1:0JzlMkj0TRzQZfJkVvzbP0HBR3IKzErnv2BNG4W4MAM=
 github.com/pkg/errors v0.9.1 h1:FEBLx1zS214owpjy7qsBeixbURkuhQAwrK5UwLGTwt4=

commit 72d887585b5d2808ba36da7c230d9b5003a4883f

estargz: deprecate errorutil in favor of native errors.Join
Commit cf6d7a03bce843d1f5e814268be141c45e5511b8 introduced this package
as a replacement for `github.com/hashicorp/go-multierror`. Now that
Go stdlib provides native support for multi-errors through errors.Join,
we can deprecated the utility, and use stdlib errors instead. Using
errors.Join will produce a slightly different formatting, but should
still capture the same information.

There do not appear to be external consumers of this package, other than
through vendoring, so we could decide to remove the package entirely;
https://grep.app/search?f.lang=Go&q=estargz%2Ferrorutil

Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

estargz/build.go
@@ -37,7 +37,6 @@ import (
 	"sync"
 	"sync/atomic"
 
-	"github.com/containerd/stargz-snapshotter/estargz/errorutil"
 	"github.com/klauspost/compress/zstd"
 	digest "github.com/opencontainers/go-digest"
 	"golang.org/x/sync/errgroup"
@@ -656,7 +655,7 @@ func (tf *tempFiles) cleanupAll() error {
 		}
 	}
 	tf.files = nil
-	return errorutil.Aggregate(allErr)
+	return errors.Join(allErr...)
 }
 
 func newCountReadSeeker(r io.ReaderAt) (*countReadSeeker, error) {

estargz/errorutil/errors.go
@@ -23,6 +23,8 @@ import (
 )
 
 // Aggregate combines a list of errors into a single new error.
+//
+// Deprecated: use [errors.Join] instead. This package will be removed in v0.19.0
 func Aggregate(errs []error) error {
 	switch len(errs) {
 	case 0:

estargz/estargz.go
@@ -38,7 +38,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/containerd/stargz-snapshotter/estargz/errorutil"
 	digest "github.com/opencontainers/go-digest"
 	"github.com/vbatts/tar-split/archive/tar"
 )
@@ -164,7 +163,7 @@ func Open(sr *io.SectionReader, opt ...OpenOption) (*Reader, error) {
 		allErr = append(allErr, err)
 	}
 	if !found {
-		return nil, errorutil.Aggregate(allErr)
+		return nil, errors.Join(allErr...)
 	}
 	if err := r.initFields(); err != nil {
 		return nil, fmt.Errorf("failed to initialize fields of entries: %v", err)
@@ -192,7 +191,7 @@ func OpenFooter(sr *io.SectionReader) (tocOffset int64, footerSize int64, rErr e
 		}
 		allErr = append(allErr, err)
 	}
-	return 0, 0, errorutil.Aggregate(allErr)
+	return 0, 0, errors.Join(allErr...)
 }
 
 // initFields populates the Reader from r.toc after decoding it from

estargz/testutil.go
@@ -40,7 +40,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/containerd/stargz-snapshotter/estargz/errorutil"
 	"github.com/klauspost/compress/zstd"
 	digest "github.com/opencontainers/go-digest"
 )
@@ -1650,7 +1649,7 @@ func newCalledTelemetry() (telemetry *Telemetry, check func(needsGetTOC bool) er
 			if !deserializeTocLatencyCalled {
 				allErr = append(allErr, fmt.Errorf("metrics DeserializeTocLatency isn't called"))
 			}
-			return errorutil.Aggregate(allErr)
+			return errors.Join(allErr...)
 		}
 }
 